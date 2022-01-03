37c85c056ae3e5612af8932e0809a383889ea880
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-03 16:21:05 -0500

    intialize players and decider in main file

--- main.rb
+++ main.rb
@@ -1,12 +1,13 @@
 require './player'
 require 'pp'
+require './quiz'
+require './decider'
 
 def init
   player1 = Player.new('player1')
   player2 = Player.new('player2')
-
-  pp player1
-  pp player2
+  decider = Decider.new()
+  decider.init(player1,player2)
 end
 
 init()
