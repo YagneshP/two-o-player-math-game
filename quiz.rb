66c42e09f3ca89865f63158332d78231060188c2
yagnesh parekh <yagneshparekh@rocketmail.com>
2022-01-03 16:21:50 -0500

    modified class variables to instance varibales

--- quiz.rb
+++ quiz.rb
@@ -1,10 +1,13 @@
 class Quiz
-  @@randomNumber1 = rand(1..20)
-  @@randomNumber2 = rand(1..20)
-  def generateQuestion
-    puts "What does #{@@randomNumber1} plus #{@@randomNumber2} equal?"
+  attr_reader :randomNumber1, :randomNumber2
+  def initialize
+    @randomNumber1 = rand(1..20)
+    @randomNumber2 = rand(1..20)
+  end
+  def generateQuestion(player_name)
+    puts "#{player_name} :What does #{@randomNumber1} plus #{@randomNumber2} equal?"
   end
   def correctAnswer
-    @@randomNumber1 + @@randomNumber2
+    @randomNumber1 + @randomNumber2
   end
 end
